680e86ac7cf3dd5807bc92ce325823113bf4838c
Fahim Ali Zain <fahimalizain@gmail.com>
2022-03-15 07:50:10 +0530

    feat: Bill Pay Dialog

--- amh_rentals_erpnext/amh_rentals___erpnext/doctype/rental_voucher/rental_voucher.js
+++ amh_rentals_erpnext/amh_rentals___erpnext/doctype/rental_voucher/rental_voucher.js
@@ -147,6 +147,125 @@ frappe.ui.form.on("Rental Voucher", {
     });
     d.show();
   },
+
+  show_bill_form(frm) {
+    const table_fields = [
+      {
+        label: "Item",
+        fieldtype: "Link",
+        fieldname: "item",
+        options: "Item",
+        reqd: 1,
+        in_list_view: 1,
+        read_only: 1,
+      },
+      {
+        label: "Qty",
+        fieldname: "qty",
+        fieldtype: "Int",
+        read_only: 1,
+        in_list_view: 1,
+      },
+      {
+        label: "Days Taken",
+        fieldname: "days_taken",
+        fieldtype: "Int",
+        onchange: () => onChange(d),
+        reqd: 0,
+        in_list_view: 1,
+      },
+      {
+        label: "Daily Rate",
+        fieldname: "daily_rate",
+        fieldtype: "Currency",
+        onchange: () => onChange(d),
+        reqd: 0,
+        in_list_view: 1,
+      },
+      {
+        label: "Amount",
+        fieldname: "amount",
+        fieldtype: "Currency",
+        read_only: 1,
+        in_list_view: 1,
+      },
+    ];
+
+    const default_rows = frm.doc.items.map((x, idx) => ({
+      name: "row " + idx,
+      __islocal: true,
+      item: x.item,
+      qty: x.qty,
+      days_taken: x.days_taken,
+      daily_rate: x.daily_rate,
+      amount: x.amount,
+    }));
+
+    const onChange = (d) => {
+      let total = 0;
+      for (const item of d.get_values().items) {
+        item.amount = item.qty * item.days_taken * item.daily_rate;
+        total += item.amount;
+      }
+      d.fields_dict["items"].grid.refresh();
+      d.set_value("total", total)
+      d.set_value("grand_total", total - d.get_values().discount);
+    }
+
+    const d = new frappe.ui.Dialog({
+      title: "Finalize Bill",
+      fields: [
+        {
+          fieldname: "items",
+          fieldtype: "Table",
+          fields: table_fields,
+          data: default_rows,
+          cannot_add_rows: true,
+          cannot_delete_rows: true,
+        },
+        { fieldtype: "Section Break" },
+        {
+          fieldname: "total",
+          fieldtype: "Currency",
+          default: frm.doc.total,
+          read_only: 1,
+          label: "Total",
+        },
+        {
+          fieldname: "discount",
+          fieldtype: "Currency",
+          default: frm.doc.discount,
+          onchange: () => onChange(d),
+          label: "Discount",
+        },
+        { fieldtype: "Column Break" },
+        {
+          fieldname: "grand_total",
+          fieldtype: "Currency",
+          read_only: 1,
+          default: frm.doc.grand_total,
+          label: "Grand Total",
+        },
+      ],
+      primary_action_label: "Finalize Bill",
+      primary_action(values) {
+        frm.call({
+          method: "make_return",
+          args: values,
+          doc: frm.doc,
+          callback(r) {
+            if (r.exc) {
+              frappe.msgprint("Something went wrong! Please contact developer");
+            } else {
+              frappe.msgprint("Return made successfully");
+              d.hide();
+            }
+          },
+        });
+      },
+    });
+    d.show();
+  },
 });
 
 frappe.ui.form.on("Rental Voucher Item", {
